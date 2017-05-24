dad147844e63aaee0e74513acf690602f8066089
Sergey Okinchuk <serg3d@hotmail.com>
2017-05-23 18:05:30 -0700

    updated

--- src/sandBox/StringReverse.java
+++ src/sandBox/StringReverse.java
@@ -7,13 +7,22 @@ public class StringReverse {
 
     public static void main(String[] args) {
         StringReverse s = new StringReverse();
-        System.out.println(s.reverse("Hello!"));
+        System.out.println(s.reverse1("Hello!"));
 
     }
 
     String reverse(String str) {
         if (str.length() == 1) return str;
         return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    String reverse1(String str) {
+        StringBuilder result = new StringBuilder();
+        for (char i : str.toCharArray()) {
+            result.insert(0, i);
+        }
+
+        return result.toString();
     }
 
 }
--- src/sandBox/TreeSide.java
+++ src/sandBox/TreeSide.java
@@ -1,5 +1,7 @@
 package sandBox;
 
+import java.util.Random;
+
 /**
  * Created by sergeyo on 4/17/2017.
  */
@@ -20,25 +22,68 @@ class Node {
         this.right = null;
         this.data = 0;
     }
-    Node (Node left, Node right, int data) {
-        if (left != null) this.left = left;
-        else this.left = null;
-
-        if (right != null) this.right = left;
-        else this.right = null;
+    Node (int data) {
+        this.data = data;
     }
 }
+
+
 
 public class TreeSide {
     public static void main(String[] args) {
-        Node randomTree = createRandomTree();
+        Node randomTree = createRandomTree(5);
+        printTree(randomTree);
 
     }
 
-    private static Node createRandomTree() {
+    public static void generateTree(int[] data) {
 
-        return null;
+
     }
 
+    private static void printTree(Node randomTree) {
+        System.out.println(randomTree.data);
 
+        if(randomTree.left != null) {
+            System.out.print("left ");
+            printTree(randomTree.left);
+
+        }
+        if(randomTree.right != null) {
+            System.out.print("right ");
+            printTree(randomTree.right);
+        }
+
+    }
+
+    private static Node createRandomTree(int nodes) {
+        Random rnd = new Random();
+        Node node = new Node(rnd.nextInt(100));
+        Node head = node;
+
+        for (int i = 0; i < nodes; i++) {
+            node = head; // point node back to head, start from top
+            // 0 - left, 1 - right
+            int side = rnd.nextInt(1);
+
+            while ((side == 0 && node.left != null) || (side == 1 && node.right != null)) {
+                side = rnd.nextInt(1);
+                if (side == 0) {
+                    node = node.left;
+                }
+                else {
+                    node = node.right;
+                }
+            } // if next node == null, insert new node there
+
+            if (side == 0) {
+                node.left = new Node(rnd.nextInt(100));
+            }
+            else {
+                node.right = new Node(rnd.nextInt(100));
+            }
+        }
+        return head;
+    }
 }
+
